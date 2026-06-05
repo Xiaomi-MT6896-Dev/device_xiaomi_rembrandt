9dd2b4ca557d0b6ba8ec42ba5e4d3e208eb3004f
mmqmemmm <rdgbnirr3418435@163.com>
2026-06-06 00:32:11 +0800

    rembrandt: Specify using beanpod
    Change-Id: I3e883eb5559aad6448dadd03abe02443475cab58

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,9 +6,6 @@
 
 DEVICE_PATH := device/xiaomi/rembrandt
 
-# Inherit from mt6895-common
-include device/xiaomi/mt6895-common/BoardConfigCommon.mk
-
 # Display
 TARGET_SCREEN_DENSITY := 560
 
@@ -26,3 +23,6 @@ VENDOR_SECURITY_PATCH := 2024-11-01
 
 # Inherit the proprietary files
 include vendor/xiaomi/rembrandt/BoardConfigVendor.mk
+
+# Inherit from mt6895-common
+include device/xiaomi/mt6895-common/BoardConfigCommon.mk
--- device.mk
+++ device.mk
@@ -4,9 +4,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from mt6895-common
-$(call inherit-product, device/xiaomi/mt6895-common/mt6895.mk)
-
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
     bluetooth.device.default_name=Redmi K60E
@@ -55,5 +52,11 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# TEE
+BOARD_TEE_VARIANT := beanpod
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/rembrandt/rembrandt-vendor.mk)
+
+# Inherit from mt6895-common
+$(call inherit-product, device/xiaomi/mt6895-common/mt6895.mk)
